36f34d6d54a2fc119e343f60623cf7a0d4eb4641
FWinkler79 <52044081+FWinkler79@users.noreply.github.com>
2019-11-19 13:48:12 +0100

    Added lombok sample, for those who think Java is too verbose...

---
+++ sample-application/src/main/java/com/sap/spring/sample/application/lombok/SimpleApplication.java
@@ -0,0 +1,22 @@
+package com.sap.spring.sample.application.lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SimpleApplication {
+
+	public static void main(String[] args) {
+	  ConfigurableApplicationContext applicationContext = SpringApplication.run(SimpleApplication.class, args);
+		
+	  Customer customer = applicationContext.getBean(Customer.class);
+	  System.out.println("Customer: " + customer);
+	}
+
+	@Bean
+	Customer customer() {
+	  return new Customer("Alan Turing");
+	}
+}
